39f0b402b980013408301ff53108af7e73f7e690
Marino Bonetti <marinobonetti@gmail.com>
2019-07-18 16:15:41 +0200

    jquery come dipendenza del js

--- app/assets/javascripts/kono_utils_bootstrap_view4/application.js
+++ app/assets/javascripts/kono_utils_bootstrap_view4/application.js
@@ -1,3 +1,4 @@
+//= require jquery/dist/jquery
 //= require bootstrap/dist/js/bootstrap
 
 
--- spec/dummy/app/assets/javascripts/application.js
+++ spec/dummy/app/assets/javascripts/application.js
@@ -11,7 +11,5 @@
 // about supported directives.
 //
 //= require rails-ujs
-
-//= require jquery/dist/jquery
 //= require kono_utils_bootstrap_view4/application
 //= require_tree .
