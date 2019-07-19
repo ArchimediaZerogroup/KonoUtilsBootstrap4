e2e18cbeadfbd4e340d80b663e942f147ac142cb
Marino Bonetti <marinobonetti@gmail.com>
2019-07-19 15:51:51 +0200

    dummy

--- spec/dummy/config/application.rb
+++ spec/dummy/config/application.rb
@@ -26,5 +26,7 @@ module Dummy
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    config.i18n.default_locale = :it
+    I18n.available_locales = [:it]
   end
 end
