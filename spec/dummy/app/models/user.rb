85b5b1204b54ef966683cfb1f9fc08dfc82850f0
Marino Bonetti <marinobonetti@gmail.com>
2019-07-22 12:58:34 +0200

    modello dummy

--- spec/dummy/app/models/user.rb
+++ spec/dummy/app/models/user.rb
@@ -21,6 +21,10 @@ class User < ApplicationRecord
 
   belongs_to :category
 
+  has_many :user_tags, :dependent => :destroy
+  has_many :tags, :through => :user_tags
+
+
   has_many :contacts
   accepts_nested_attributes_for :contacts
 
--- spec/dummy/app/models/user_tag.rb
+++ spec/dummy/app/models/user_tag.rb
@@ -10,4 +10,8 @@
 #
 
 class UserTag < ApplicationRecord
+
+  belongs_to :user
+  belongs_to :tag
+
 end
