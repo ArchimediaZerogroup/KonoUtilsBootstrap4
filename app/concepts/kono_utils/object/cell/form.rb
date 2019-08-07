b55c1e5c5ce992620f9dfc09a1ab91d4b90ef0e0
Marino Bonetti <marinobonetti@gmail.com>
2019-08-07 15:04:09 +0200

    documentazione

--- app/concepts/kono_utils/object/cell/form.rb
+++ app/concepts/kono_utils/object/cell/form.rb
@@ -7,7 +7,14 @@ module KonoUtils::Object::Cell # namespace
     include ActionView::Helpers::DateHelper
     include ActionView::Helpers::FormOptionsHelper
 
-
+    ##
+    # Metodo che mi ritorna la lista degli argomenti da passare a simple form.
+    # @return [Array]
+    #
+    # ES:
+    # [model] -> model deve essere l'ActiveRecord da utilizzare come oggetto della form
+    #
+    # [model,{ url:helper_path_custom }] -> come secondo parametro abbiamo l'hash delle opzioni da poter passare al form builder
     def forms_options
       [model]
     end
