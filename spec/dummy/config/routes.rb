e058571147c5a131485ebfe3b9edc30e46c623df
Marino Bonetti <marinobonetti@gmail.com>
2019-08-07 11:54:13 +0200

    rotta per dummy contacts

--- spec/dummy/config/routes.rb
+++ spec/dummy/config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  resources :users
+  resources :users do
+    resources :contacts, only: [:create,:new]
+  end
+
   resources :contacts
 
   root to: "users#index"
