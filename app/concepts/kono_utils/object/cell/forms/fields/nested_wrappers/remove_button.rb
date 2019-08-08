4b1b8d8203efc227a2a8a07f9e7949eeeb67bbc5
Marino Bonetti <marinobonetti@gmail.com>
2019-08-08 14:20:59 +0200

    implementazione iniziale remove button

---
+++ app/concepts/kono_utils/object/cell/forms/fields/nested_wrappers/remove_button.rb
@@ -0,0 +1,35 @@
+module KonoUtils::Object::Cell::Forms::Fields # namespace
+  ##
+  # Bottone per la rimozione della riga nested
+  class NestedWrappers::RemoveButton < KonoUtils::Object::Cell::Buttons::Delete
+
+
+    def url_to
+      "#"
+    end
+
+    ##
+    # Nome della funzione da utilizzare quando viene cliccato il bottone
+    def js_function_name
+      @_fn_name ||= "remove_#{SecureRandom.hex(10)}"
+    end
+
+    # def btn_opts(opts = {})
+    #   custom_options = options.dig(:button_options) || opts
+    #   opts = super({
+    #                  onclick: "#{js_function_name}()",
+    #                }.merge(custom_options))
+    #
+    #   opts.delete(:method)
+    #   opts[:data].delete(:confirm)
+    #
+    #   opts
+    # end
+
+    def show(&block)
+      super + render(&block).html_safe
+    end
+
+
+  end
+end
