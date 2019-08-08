5a5af7a873175dc58b5d432ca85de077557ac27f
Marino Bonetti <marinobonetti@gmail.com>
2019-08-08 16:36:18 +0200

    corretto dummy

--- spec/dummy/app/concepts/contacts/cell/forms/fields/layout.rb
+++
@@ -1,7 +0,0 @@
-class Contacts::Cell::Forms::Fields::Layout < KonoUtils::Object::Cell::Forms::Fields::Layout
-
-  def cell_column_class
-    "col-md-9 offset-md-2"
-  end
-
-end
--- spec/dummy/config/routes.rb
+++ spec/dummy/config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   resources :users do
-    resources :contacts, module: :users, only: [:create, :new]
+    namespace :users do
+      resources :contacts, only: [:create, :new]
+    end
   end
 
   resources :contacts
