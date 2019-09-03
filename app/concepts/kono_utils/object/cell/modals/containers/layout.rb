18964c9111ccbb29a4f16ee59b19e246e244a80b
Marino Bonetti <marinobonetti@gmail.com>
2019-09-03 13:38:23 +0200

    modifica per modal e renderizzazione componenti

--- app/concepts/kono_utils/object/cell/modals/containers/layout.rb
+++ app/concepts/kono_utils/object/cell/modals/containers/layout.rb
@@ -2,6 +2,9 @@ module KonoUtils::Object::Cell::Modals # namespace
   # Layout Standard del contenitore della modal
   class Containers::Layout < Base
 
+    def modal_container_id
+      context[:modal_container_id]
+    end
 
   end
 end
