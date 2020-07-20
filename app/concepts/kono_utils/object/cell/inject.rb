85bbc8a04ba43af1d4db0e8f87af34f7253865d9
Marino Bonetti <marinobonetti@gmail.com>
2020-07-20 15:03:28 +0200

    update corretto

--- app/concepts/kono_utils/object/cell/inject.rb
+++ app/concepts/kono_utils/object/cell/inject.rb
@@ -5,6 +5,7 @@ module KonoUtils::Object::Cell # namespace
   #   html_manipulation_method: [(:append),:prepend,:replaceWith] ->  definisce come deve operare il javascript
   #                                                             nell'operazione di manipolazione del contenitore
   #                                                             in cui inserire il contenuto
+  #   inject_show_modal:  [Boolean]  -> identifica se stiamo visualizzando la show della modal o meno, default a false
   class Inject < Base
 
     load('action_view/helpers/form_helper.rb')
@@ -31,29 +32,19 @@ module KonoUtils::Object::Cell # namespace
     end
 
     def form_content
+      #nel caso stiamo visualizzando il rimpiazzo dell'output delle modals(quando per esempio abbiamo aggiornato od inserito un nuovo record)
+      if options.fetch(:inject_show_modal, false)
 
-      if inject_as_modal?
-
-        concept("cell/modals/container",
-                concept("cell/form", model,
-                        form_options: {remote: true},
-                        context: {
-                          nested_layout: nested_layout,
-                          modal_upgraded_target_container: modal_upgraded_target_container
-                        }.merge(context)),
-                layout: model.class.layout_ns("cell/modals/containers/layout")
-        )
-
-      else
         container = nil
         simple_form_for(model, as: "#{params.dig(:kono_utils, :structure_nested)}[#{Time.now.to_i}]", url: '#') do |form|
 
-          container = concept("cell/forms/fields/nested_wrappers/form", model,
-                              show_remove_button: !inject_as_modal?, #per le modal non visualizziamo direttamente il bottone
-                              layout: form_layout,
+          container = concept("cell/forms/fields/nested_modal_wrappers/show", model,
+                              show_remove_button: true,
+                              layout: layout_ns("cell/forms/fields/nested_wrappers/forms/layout"),
                               context: {
                                 form: form,
                                 base_class: base_class,
+                                nested_layout: nested_layout,
                                 current_user: context[:current_user]
                               }.merge(remote_context)
           )
@@ -62,11 +53,48 @@ module KonoUtils::Object::Cell # namespace
 
         container
 
+      else
+        if inject_as_modal?
+
+          concept("cell/modals/container",
+                  concept("cell/form", model,
+                          form_options: {remote: true},
+                          context: {
+                            nested_layout: nested_layout,
+                            modal_upgraded_target_container: params.dig(:kono_utils, :modal_upgraded_target_container)
+                          }.merge(context)),
+                  layout: model.class.layout_ns("cell/modals/containers/layout")
+          )
+
+        else
+          container = nil
+          simple_form_for(model, as: "#{params.dig(:kono_utils, :structure_nested)}[#{Time.now.to_i}]", url: '#') do |form|
+
+            container = concept("cell/forms/fields/nested_wrappers/form", model,
+                                show_remove_button: !inject_as_modal?, #per le modal non visualizziamo direttamente il bottone
+                                layout: form_layout,
+                                context: {
+                                  form: form,
+                                  base_class: base_class,
+                                  current_user: context[:current_user]
+                                }.merge(remote_context)
+            )
+
+          end
+
+          container
+
+        end
       end
     end
 
+
     def target_container
-      params.dig(:kono_utils, :target_container)
+      if options.fetch(:inject_show_modal, false)
+        "##{params.dig(:kono_utils, :modal_upgraded_target_container)}"
+      else
+        params.dig(:kono_utils, :target_container)
+      end
     end
 
     def html_manipulation_method
@@ -79,12 +107,6 @@ module KonoUtils::Object::Cell # namespace
       params.dig(:kono_utils, :inject_as_modal)
     end
 
-    ##
-    # ID Container nella form iniziale, che dovrà essere aggiornato una volta completata l'operazione di edit, aggiornando
-    # con lo show della cella il suo contenuto
-    def modal_upgraded_target_container
-      params.dig(:kono_utils, :modal_upgraded_target_container)
-    end
 
     private
 
