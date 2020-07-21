53de91da3d27c0a5b0f0031f2e8278a14ef0eb5d
Marino Bonetti <marinobonetti@gmail.com>
2020-07-21 09:20:56 +0200

    presumibile refuso

--- app/concepts/kono_utils/object/cell/forms/fields/nested_modal_wrappers/show.rb
+++ app/concepts/kono_utils/object/cell/forms/fields/nested_modal_wrappers/show.rb
@@ -5,9 +5,10 @@ module KonoUtils::Object::Cell::Forms::Fields # namespace
   class NestedModalWrappers::Show < NestedWrappers::Form
 
     # nella show dobbiamo renderizzare nascosti solo i campi dell'ID del destroy
-    def form_attributes
-      super.select { |v| v.name == :id or v.name == :_destroy }
-    end
+    # FIXME serve questo codice?
+    # def form_attributes
+    #   super.select { |v| v.name == :id or v.name == :_destroy }
+    # end
 
   end
 end
