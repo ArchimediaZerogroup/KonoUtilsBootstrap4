1f4e90568216e679ac399f25dbc7b06ba1f716db
Marino Bonetti <marinobonetti@gmail.com>
2021-07-19 16:04:53 +0200

    ritorno della policy cachata per la riga della tabella

--- app/concepts/kono_utils/object/cell/index/row_buttons.rb
+++ app/concepts/kono_utils/object/cell/index/row_buttons.rb
@@ -4,6 +4,11 @@ module KonoUtils
       module Index # namespace
         class RowButtons < Base # class
 
+          ##
+          # Restituisce la policy per la riga, non viene instanziata più volte per oggetto
+          def cached_policy
+            @_cached_policy ||= policy(model)
+          end
         end
       end
     end
